6208bd8b282abaea6e8e61bc3d904958ecbfb770
marcosmoresco <marcoshoppusmoresco@gmail.com>
2016-04-18 10:20:22 -0300

    alter port

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var server = require('./config/server.js'),
   logger = require('./config/logger.js'),
   routes = require('./config/routes.js'),
-  statics = require('./config/statics.js');
-
-/*
- * Configuration of statics files
- */
-statics.configStatics();
+  mail = require('./config/mail.js');
 
 /*
  * Run server
  */
-server.listen(3000, function() {
+
+server.listen(5000, function() {
   logger.info('%s listening at %s', server.name, server.url);
 });
